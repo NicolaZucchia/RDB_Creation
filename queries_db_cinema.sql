5758518ab2db6f980f757c9fde12c66711aca13b
NicolaZucchia <65070282+NicolaZucchia@users.noreply.github.com>
2023-01-11 18:28:08 +0100

    Delete queries_db_cinema.sql

--- queries_db_cinema.sql
+++
@@ -1,7 +0,0 @@
-delimiter $$
-create procedure RiempimentoGiornaliero()
-begin
-    select count(*) as 'Conteggio', sala from visione inner join proiezione
-    where visione.salaProiezione = proiezione.sala group by sala;
-end $$
-delimiter ;
